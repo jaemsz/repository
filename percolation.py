797b2286c11e5ca47bcd3985f8f49a69a7912835
jaemsz <68905231+jaemsz@users.noreply.github.com>
2020-07-28 13:36:07 -0700

    Update percolation.py
    Code review fixes

--- percolation.py
+++ percolation.py
@@ -5,14 +5,17 @@ class Percolation:
     def __init__(self, n):
         # n x n matrix
         self.n = n
-                
-        # number of open sites
-        self.openSiteCount = 0
-
+        
         # first and last rows
         self.first_row = 1
         self.last_row = n+1
         
+        # number of open sites
+        self.openSiteCount = 0
+
+        # number of components
+        self.componentsCount = (n+2)*n
+
         # create 2 additional rows for virtual top and bottom sites
         self.grid = [[0 for c in range(n)] for r in range(n+2)]
         
@@ -20,29 +23,20 @@ class Percolation:
         self.id = [x for x in range((n+2)*n)]
         self.sz = [1 for x in range((n+2)*n)]
         
-        # number of components
-        self.components = (n+2)*n
-
         # define virtual top site to be at [0,n//2]
-        self.virtual_top_site_id = self.gridToId(0,n//2)
+        self.virtual_top_site_id = self.__gridToId(0,n//2)
         
         # define virtual bottom site to be at [n-1,n//2]
-        self.virtual_bottom_site_id = self.gridToId(n+1,n//2)
+        self.virtual_bottom_site_id = self.__gridToId(n+1,n//2)
         
         # connect virtual top and bottom sites 
         for c in range(n):
-            self.union(self.virtual_top_site_id, self.gridToId(1,c))
-            self.union(self.virtual_bottom_site_id, self.gridToId(n,c))
-        
-    # @param row : int value of row
-    # @param col : int value of col
-    # @return id index of row, col
-    def gridToId(self, row, col):
-        return row * 10 + col
+            self.__union(self.virtual_top_site_id, self.__gridToId(1,c))
+            self.__union(self.virtual_bottom_site_id, self.__gridToId(n,c))
     
     # @param p : int value of id index
     # @return an int value representing the root of p
-    def root(self, p):
+    def __root(self, p):
         while p != self.id[p]:
             self.id[p] = self.id[self.id[p]] # path compression
             p = self.id[p]
@@ -50,10 +44,10 @@ class Percolation:
     
     # @param p : int value of id index
     # @param q : int value of id index
-    def union(self, p, q):
+    def __union(self, p, q):
         # get the root of p and q
-        root_p = self.root(p)
-        root_q = self.root(q)
+        root_p = self.__root(p)
+        root_q = self.__root(q)
         
         if root_p == root_q:
             return
@@ -67,24 +61,29 @@ class Percolation:
             self.sz[root_p] += self.sz[root_q]
             
         # every time a union is performed we decrease the number of components by 1
-        self.components -= 1
+        self.componentsCount -= 1
         
     # @param p : int value of id index
     # @param q : int value of id index
     # @return True if p and q are connected otherwise False
-    def isConnected(self, p, q):
-        return self.root(p) == self.root(q)
+    def __isConnectedId(self, p, q):
+        return self.__root(p) == self.__root(q)
     
-    # @return number of components
-    def componentsCount(self):
-        return self.components
-        
+    # @param row : int value of row
+    # @param col : int value of col
+    # @return id index of row, col
+    def __gridToId(self, row, col):
+        return row * 10 + col
+    
     # @param row : int value of grid row
     # @param col : int value of grid col
     def open(self, row, col):
         if row < self.first_row or row >= self.last_row or col < 0 or col >= self.n:
             raise ValueError('row < self.first_row or row >= self.last_row or col < 0 or col >= self.n')
         
+        if self.grid[row][col] == 1:
+            return
+            
         self.grid[row][col] = 1
         
         dr = [ 0, 0, -1, 1]
@@ -97,9 +96,9 @@ class Percolation:
             if next_row > 0 and next_row < self.last_row and next_col >= 0 and next_col < self.n:
                 
                 if self.grid[next_row][next_col] == 1:
-                    p = self.gridToId(row,col)
-                    q = self.gridToId(next_row,next_col)
-                    self.union(p,q)
+                    p = self.__gridToId(row,col)
+                    q = self.__gridToId(next_row,next_col)
+                    self.__union(p,q)
             
         self.openSiteCount += 1
         
@@ -120,16 +119,30 @@ class Percolation:
             raise ValueError('row < self.first_row or row >= self.last_row or col < 0 or col >= self.n')
 
         # convert to Id value
-        p = self.gridToId(row,col)
+        p = self.__gridToId(row,col)
         
         # is grid[row][col] connected to virtual top site
-        return self.isConnected(p, self.virtual_top_site_id)
+        return self.__isConnectedId(p, self.virtual_top_site_id)
     
+    # @param row : int value of grid row
+    # @param col : int value of grid col
+    # @param row : int value of grid row
+    # @param col : int value of grid col
+    # @return True if the two grid location are connected
+    def isConnected(self, row, col, row2, col2):
+        p = self.__gridToId(row, col)
+        q = self.__gridToId(row2, col2)
+        return self.__isConnectedId(p, q)
+    
+    # @return True if there is a connection from virtual top site to virtual bottom site
+    def percolates(self):
+        return self.__isConnectedId(self.virtual_top_site_id, self.virtual_bottom_site_id)
+
+    # @return number of components
+    def numberOfcomponents(self):
+        return self.componentsCount
+
     # @return the number of open sites
     def numberOfOpenSites(self):
         return self.openSiteCount
     
-    # @return True if there is a connection from virtual top site to virtual bottom site
-    def percolates(self):
-        return self.isConnected(self.virtual_top_site_id, self.virtual_bottom_site_id)
-        
