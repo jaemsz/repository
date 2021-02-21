07357e0b4414f76b38585bc35d65a4464ef2509e
jaemsz <68905231+jaemsz@users.noreply.github.com>
2021-02-21 01:06:15 -0800

    Update netconn.py

--- netconn.py
+++ netconn.py
@@ -2,6 +2,13 @@ from ctypes import *
 import socket
 import struct
 import psutil
+import json
+import hashlib
+import os
+import sys
+
+OUTPUT_NORMAL = 0
+OUTPUT_JSON = 1
 
 NO_ERROR = 0
 ERROR_INSUFFICIENT_BUFFER = 122
@@ -72,9 +79,23 @@ def state_to_string(state):
     else:
         return "<UNKNOWN>"
         
-def main():
+def get_sha256(command_line):
+    if not command_line:
+        return "<UNKNOWN>"
+    else:
+        file_path = command_line[0]
+        dir_path = os.path.dirname(file_path)
+        if not dir_path:
+            return "<UNKNOWN>"
+        else:
+            with open(file_path, "rb") as f:
+                contents = f.read()
+                sha256 = hashlib.sha256(contents).hexdigest()
+            return sha256
+    
+def main(output_type):
     pid_ip_map = {}
-
+    
     windll.iphlpapi.GetTcpTable2.argtypes = [c_void_p, POINTER(c_ulong), c_bool]
     tcp_table_size = c_ulong()
     
@@ -93,26 +114,45 @@ def main():
                 dst_port = tcp_table.table[i].dwRemotePort
                 pid = tcp_table.table[i].dwOwningPid
                 state = state_to_string(tcp_table.table[i].dwState)
+                cmd_line = psutil.Process(pid).cmdline()
+                sha256 = get_sha256(cmd_line)
+                
                 if pid not in pid_ip_map:
-                    pid_ip_map[pid] = [{
-                        "src" : src_ip + ":" + str(src_port),
-                        "dst" : dst_ip + ":" + str(dst_port), 
-                        "state" : state,
-                    }]
+                    pid_ip_map[pid] = {
+                        "filename" : psutil.Process(pid).name(),
+                        "cmd_line" : cmd_line,
+                        "sha256" : sha256,                        
+                        "connections" : [
+                            {
+                                "src" : src_ip + ":" + str(src_port),
+                                "dst" : dst_ip + ":" + str(dst_port), 
+                                "state" : state,
+                            }
+                        ]
+                    }
                 else:
-                    pid_ip_map[pid].append({
-                        "src" : src_ip + ":" + str(src_port),
-                        "dst" : dst_ip + ":" + str(dst_port), 
-                        "state" : state,
-                    })
+                    pid_ip_map[pid]["connections"].append(
+                        {
+                            "src" : src_ip + ":" + str(src_port),
+                            "dst" : dst_ip + ":" + str(dst_port), 
+                            "state" : state,
+                        }
+                    )
     
     # print out the TCP connections
-    for pid in pid_ip_map:
-        print("PID: " + str(pid))
-        print("Process Name: " + psutil.Process(pid).name())
-        for ip in pid_ip_map[pid]:
-            print("{:10}{:25}{:10}{:25}{:10}{:25}".format("SRC: ", ip.get("src"), "DST: ", ip.get("dst"), "STATE: ", ip.get("state")))
-        print()
+    if output_type == OUTPUT_NORMAL:
+        for pid in pid_ip_map:
+            print("PID: " + str(pid))
+            print("Process Name: " + psutil.Process(pid).name())
+            print("SHA256: " + pid_ip_map[pid]["sha256"])
+            for c in pid_ip_map[pid]["connections"]:
+                print("{:10}{:25}{:10}{:25}{:10}{:25}".format("SRC: ", c.get("src"), "DST: ", c.get("dst"), "STATE: ", c.get("state")))
+            print()
+    else:
+        print(json.dumps(pid_ip_map, indent=4))
     
 if __name__ == "__main__":
-    main()
+    output_type = OUTPUT_NORMAL
+    if len(sys.argv) == 2 and sys.argv[1] == "JSON":
+        output_type = OUTPUT_JSON
+    main(output_type)
