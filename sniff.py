6bd749620b8b6c3ca3e78143ae79d9c039fa2919
jaemsz <68905231+jaemsz@users.noreply.github.com>
2021-02-22 20:04:49 -0800

    Update sniff.py

--- sniff.py
+++ sniff.py
@@ -37,16 +37,14 @@ def get_process_name(sip, dip, sport, dport):
         tcp_table = MIB_TCPTABLE2_FACTORY(tcp_table_size.value)
         
         ret = windll.iphlpapi.GetTcpTable2(byref(tcp_table), byref(tcp_table_size), True)
-        if ret != NO_ERROR:
-            print("ERROR: GetTcpTable2() failed, error = " + str(ret))
-        else:
+        if ret == NO_ERROR:
             for i in range(tcp_table.dwNumEntries):
                 pid = tcp_table.table[i].dwOwningPid
                 src_ip = socket.inet_ntoa(struct.pack('<L', tcp_table.table[i].dwLocalAddr))
                 dst_ip = socket.inet_ntoa(struct.pack('<L', tcp_table.table[i].dwRemoteAddr))
                 src_port = socket.ntohs(tcp_table.table[i].dwLocalPort)
                 dst_port = socket.ntohs(tcp_table.table[i].dwRemotePort)
-                if src_ip == sip and dst_ip == dip and src_port == sport and dst_port == dport:
+                if src_port == sport and dst_port == dport and src_ip == sip and dst_ip == dport:
                     try:
                         return psutil.Process(pid).name(), pid
                     except psutil.NoSuchProcess:
@@ -57,47 +55,48 @@ def get_process_name(sip, dip, sport, dport):
         return "<UNKNOWN>", pid
     
 def on_packet(pkt):
-    protocol = ""
+    protocol = []
         
     src_port = 0
     dst_port = 0
     if TCP in pkt:
         src_port = pkt[TCP].sport
         dst_port = pkt[TCP].dport
-        protocol += "TCP,"
+        protocol.append("TCP")
     elif UDP in pkt:
         src_port = pkt[UDP].sport
         dst_port = pkt[UDP].dport
-        protocol += "UDP,"
+        protocol.append("UDP")
     
     name = ""
     if DNS in pkt:
         if pkt.qdcount > 0 and isinstance(pkt.qd, DNSQR):
             name = pkt[DNS].qd.qname
             name = name.decode("utf-8")
-            protocol += "DNSQR,"
+            protocol.append("DNSQR")
         elif pkt.ancount > 0 and isinstance(pkt.an, DNSRR):
             name = pkt.an.rdata
             name = name.decode("utf-8")
-            protocol += "DNSRR,"
+            protocol.append("DNSRR")
 
     src_ip = ""
     dst_ip = ""
     if IP in pkt:
         src_ip = pkt[IP].src
         dst_ip = pkt[IP].dst
-        protocol += "IP"
+        protocol.append("IP")
             
     process_name, pid = get_process_name(src_ip, dst_ip, src_port, dst_port)
     
     print("{:10}{:30}{:6}{:10}{:10}{:20}{:9}{:10}{:10}{:20}{:9}{:10}{:8}{:50}{:8}{:10}".format(
-        "PROCESS:", process_name, "PID:", str(pid), 
+        "PROCESS:", process_name, 
+        "PID:", str(pid), 
         "SRC IP:", src_ip, 
         "SPORT:", str(src_port), 
         "DST IP:", dst_ip, 
         "DPORT:", str(dst_port),
         "DNS:", name, 
-        "PROT:", protocol))
+        "PROT:", ",".join(protocol)))
     
     
 def main():
